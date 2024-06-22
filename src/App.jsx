ad9ad8c4c960a0211b20899b6fecb66aa1dd1076
maneesh-settipeta <maneeshsettipeta@gmail.com>
2024-06-21 21:31:25 -0500

    updated login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,172 +1,13 @@
-import { useState } from "react";
-import DefaultPage from "./Components/DefaultPage";
 import Header from "./Components/Header";
 import SideBarRight from "./Components/SideBarRight";
-import CreateProjects from "./Components/CreateProjects";
-import SelectedProject from "./Components/SelectedProject";
-import Tasks from "./Components/Tasks";
+
 import LoginPage from "./Components/LoginPage";
-import { CreateContext } from "./Store/Store-Projects-Data";
-import { useContext } from "react";
+
+import ProjectContext from "./Store/Store-Projects-Data";
 
 function App() {
-  const [projectState, setProjectState] = useState({
-    projectStateStatus: undefined,
-    projects: [],
-    uniqueId: 0,
-    isUserLoggedIn: false,
-  });
-
-  const { tasksItems } = useContext(CreateContext);
-  console.log(tasksItems, "TaskItemsInApp.JSX");
-
-  function handleUserLoginPage(userName, userPassword) {
-    setProjectState((prevState) => {
-      let UserLoggedIn = false;
-      if (userName === "user" && userPassword === "12345") {
-        UserLoggedIn = true;
-      }
-
-      return {
-        ...prevState,
-        isUserLoggedIn: UserLoggedIn,
-      };
-    });
-  }
-
-  function handleDeleteProject(id) {
-    setProjectState((prevState) => {
-      const updatedProjects = prevState.projects.filter(
-        (project) => project.id !== id
-      );
-      return {
-        ...prevState,
-        projects: updatedProjects,
-        projectStateStatus: undefined,
-      };
-    });
-  }
-
-  function handleReturnHomePage() {
-    setProjectState((prevState) => {
-      return {
-        ...prevState,
-        projectStateStatus: undefined,
-      };
-    });
-  }
-
-  function handleOnSaveProjectID(id) {
-    setProjectState((prevState) => {
-      return {
-        ...prevState,
-        projectStateStatus: id,
-      };
-    });
-  }
-
-  function handleCancelButton() {
-    setProjectState((prevState) => {
-      return {
-        ...prevState,
-        projectStateStatus: undefined,
-      };
-    });
-  }
-
-  function handleDefaultPage() {
-    setProjectState((prevState) => {
-      return {
-        ...prevState,
-        projectStateStatus: null,
-      };
-    });
-  }
-
-  const handleRedirectCreateProjectFromSide = () => {
-    setProjectState((prevState) => {
-      return {
-        ...prevState,
-        projectStateStatus: null,
-      };
-    });
-  };
-
-  function handleTasksData(tasksItems) {
-    console.log(tasksItems, "AppRecieving");
-    setProjectState((prevState) => {
-      const projectIndex = prevState.projects.findIndex(
-        (project) => project.id === selectedprojected.id
-      );
-
-      const updatedProject = {
-        ...prevState.projects[projectIndex],
-        tasks: tasksItems,
-      };
-
-      const finalUpdated = [
-        ...prevState.projects.slice(0, projectIndex),
-        updatedProject,
-        ...prevState.projects.slice(projectIndex + 1),
-      ];
-
-      return {
-        ...prevState,
-        projects: finalUpdated,
-      };
-    });
-  }
-
-  function handleSaveData(projectDetails) {
-    setProjectState((prevState) => {
-      const newId = prevState.uniqueId + 1;
-      const newProject = {
-        ...projectDetails,
-        id: newId,
-      };
-      return {
-        ...prevState,
-        projectStateStatus: undefined,
-        projects: [...prevState.projects, newProject],
-        uniqueId: newId,
-      };
-    });
-  }
-
-  const selectedprojected = projectState.projects.find(
-    (project) => project.id === projectState.projectStateStatus
-  );
-
-  let contentRedirect;
-
-  if (projectState.projectStateStatus === null) {
-    contentRedirect = (
-      <CreateProjects
-        cancelButton={handleCancelButton}
-        onSendData={handleSaveData}
-      />
-    );
-  } else if (projectState.projectStateStatus === undefined) {
-    contentRedirect = <DefaultPage onChangeOfPage={handleDefaultPage} />;
-  } else {
-    contentRedirect = (
-      <>
-        <SelectedProject
-          // passSelectedProject={selectedprojected}
-          deleteProject={handleDeleteProject}
-          onSendTasksData={handleTasksData}
-        />
-      </>
-    );
-  }
-  const projectsData = {
-    projects: projectState.projects,
-    projectStateStatus: projectState.projectStateStatus,
-  };
-  console.log(projectsData, "App.jsx");
-
   return (
-    <CreateContext.Provider value={projectsData}>
+    <ProjectContext.Provider value={projectsData}>
       {" "}
       <main className="h-screen flex flex-col">
         {projectState.isUserLoggedIn ? (
@@ -187,7 +28,7 @@ function App() {
           <LoginPage onSendUserData={handleUserLoginPage} />
         )}
       </main>
-    </CreateContext.Provider>
+    </ProjectContext.Provider>
   );
 }
 
--- src/Store/Store-Projects-Data.jsx
+++ src/Store/Store-Projects-Data.jsx
@@ -1,6 +1,170 @@
+import { useState } from "react";
 import { createContext } from "react";
 export const CreateContext = createContext({
   projects: [],
   projectStateStatus: 1,
 });
-console.log(CreateContext.tasksItems, "StoreComp");
+
+export default ProjectContext({ children });
+{
+  const [projectState, setProjectState] = useState({
+    projectStateStatus: undefined,
+    projects: [],
+    uniqueId: 0,
+    isUserLoggedIn: false,
+  });
+
+  const { tasksItems } = useContext(CreateContext);
+  console.log(tasksItems, "TaskItemsInApp.JSX");
+
+  function handleUserLoginPage(userName, userPassword) {
+    setProjectState((prevState) => {
+      let UserLoggedIn = false;
+      if (userName === "user" && userPassword === "12345") {
+        UserLoggedIn = true;
+      }
+
+      return {
+        ...prevState,
+        isUserLoggedIn: UserLoggedIn,
+      };
+    });
+  }
+
+  function handleDeleteProject(id) {
+    setProjectState((prevState) => {
+      const updatedProjects = prevState.projects.filter(
+        (project) => project.id !== id
+      );
+      return {
+        ...prevState,
+        projects: updatedProjects,
+        projectStateStatus: undefined,
+      };
+    });
+  }
+
+  function handleReturnHomePage() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projectStateStatus: undefined,
+      };
+    });
+  }
+
+  function handleOnSaveProjectID(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projectStateStatus: id,
+      };
+    });
+  }
+
+  function handleCancelButton() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projectStateStatus: undefined,
+      };
+    });
+  }
+
+  function handleDefaultPage() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projectStateStatus: null,
+      };
+    });
+  }
+
+  const handleRedirectCreateProjectFromSide = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projectStateStatus: null,
+      };
+    });
+  };
+
+  function handleTasksData(tasksItems) {
+    console.log(tasksItems, "AppRecieving");
+    setProjectState((prevState) => {
+      const projectIndex = prevState.projects.findIndex(
+        (project) => project.id === selectedprojected.id
+      );
+
+      const updatedProject = {
+        ...prevState.projects[projectIndex],
+        tasks: tasksItems,
+      };
+
+      const finalUpdated = [
+        ...prevState.projects.slice(0, projectIndex),
+        updatedProject,
+        ...prevState.projects.slice(projectIndex + 1),
+      ];
+
+      return {
+        ...prevState,
+        projects: finalUpdated,
+      };
+    });
+  }
+
+  function handleSaveData(projectDetails) {
+    setProjectState((prevState) => {
+      const newId = prevState.uniqueId + 1;
+      const newProject = {
+        ...projectDetails,
+        id: newId,
+      };
+      return {
+        ...prevState,
+        projectStateStatus: undefined,
+        projects: [...prevState.projects, newProject],
+        uniqueId: newId,
+      };
+    });
+  }
+
+  const selectedprojected = projectState.projects.find(
+    (project) => project.id === projectState.projectStateStatus
+  );
+
+  let contentRedirect;
+
+  if (projectState.projectStateStatus === null) {
+    contentRedirect = (
+      <CreateProjects
+        cancelButton={handleCancelButton}
+        onSendData={handleSaveData}
+      />
+    );
+  } else if (projectState.projectStateStatus === undefined) {
+    contentRedirect = <DefaultPage onChangeOfPage={handleDefaultPage} />;
+  } else {
+    contentRedirect = (
+      <>
+        <SelectedProject
+          // passSelectedProject={selectedprojected}
+          deleteProject={handleDeleteProject}
+          onSendTasksData={handleTasksData}
+        />
+      </>
+    );
+  }
+  const projectsData = {
+    projects: projectState.projects,
+    projectStateStatus: projectState.projectStateStatus,
+  };
+  // console.log(projectsData, "App.jsx");
+
+  return (
+    <CreateContext.Provider value={projectsData}>
+      {children}
+    </CreateContext.Provider>
+  );
+}
