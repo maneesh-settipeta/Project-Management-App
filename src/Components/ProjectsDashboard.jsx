02badbf15c797485449f2a9ee594430d1aa45e5a
maneesh-settipeta <maneeshsettipeta@gmail.com>
2024-06-27 21:18:48 -0500

    updated login page

--- src/Components/ProjectsDashboard.jsx
+++ src/Components/ProjectsDashboard.jsx
@@ -3,30 +3,36 @@ import { CreateContext } from "../Store/Store-Projects-Data";
 
 function ProjectsDashboard() {
   const { projects, updateProjectState } = useContext(CreateContext);
-  const projectListCount = projects.length;
+
   return (
     <>
       <div className="w-5/6  bg-slate-100 flex flex-wrap overflow-y-auto ">
-        {projects?.map((project) => (
-          <div
-            key={project.id}
-            className="flex items-center"
-            onClick={() => updateProjectState(project.id)}
-          >
-            <div className="bg-white p-3 h-64 w-52 ml-9  mr-0 mb-2 mt-2 ">
-              <h1 className="text-black text-2xl mb-3">
-                <span className="font-medium text-black">Title:</span>
-                {project.title}
-              </h1>
-              <hr></hr>
-              {project?.tasks?.map((projectTask, taskIndex) => (
-                <li key={taskIndex} className="text-sky-950 text-md">
-                  {projectTask}
-                </li>
-              ))}
-            </div>
+        {projects.length === 0 ? (
+          <div className="flex w-5/6 justify-center items-center">
+            <p className="font-medium text-4xl ">No Projects </p>
           </div>
-        ))}
+        ) : (
+          projects?.map((project) => (
+            <div
+              key={project.id}
+              className="flex items-center"
+              onClick={() => updateProjectState(project.id)}
+            >
+              <div className="bg-white p-3 h-64 w-52 ml-9  mr-0 mb-2 mt-2 hover:shadow-[0_4px_10px_rgba(0,0,0,0.25)] ">
+                <h1 className="text-black text-2xl mb-3">
+                  <span className="font-medium text-black">Title:</span>
+                  {project.title}
+                </h1>
+                <hr></hr>
+                {project?.tasks?.map((projectTask, taskIndex) => (
+                  <li key={taskIndex} className="text-sky-950 text-md">
+                    {projectTask}
+                  </li>
+                ))}
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
--- src/Store/Store-Projects-Data.jsx
+++ src/Store/Store-Projects-Data.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { createContext } from "react";
 export const CreateContext = createContext({
   projects: [],
@@ -10,99 +10,131 @@ export const CreateContext = createContext({
   updateProjectState: () => {},
 });
 
-export default function ProjectContext({ children }) {
-  const [projectState, setProjectState] = useState({
-    projectStateStatus: undefined,
-    projects: [],
-    uniqueId: 0,
-    isUserLoggedIn: false,
-  });
+function ProjectsStateUseReducer(state, action) {
+  if (action.type === "ADD-PROJECT") {
+    const newId = state.uniqueId + 1;
+    const newProject = {
+      ...action.projectDetails,
+      id: newId,
+    };
+    return {
+      ...state,
+      projects: [...state.projects, newProject],
+      uniqueId: newId,
+      projectStateStatus: "returnDashboard",
+    };
+  }
+  if (action.type === "UPDATE-PROJECT") {
+    if (action?.payload?.status === "delete" && action.payload.id) {
+      const updatedProjects = state.projects.filter(
+        (project) => project.id !== state.projectStateStatus
+      );
+      console.log(action.payload.status, action.payload.id);
+      return {
+        ...state,
+        projects: updatedProjects,
+        projectStateStatus: "returnDashboard",
+      };
+    } else {
+      console.log(action.pageChange);
+      return {
+        ...state,
+        projectStateStatus: action.pageChange,
+      };
+    }
+  }
+  if (action.type === "USER-LOGIN") {
+    return {
+      ...state,
+      isUserLoggedIn: action.isUserLoggedIn,
+    };
+  }
+  if (action.type === "UPDATE-TASKS") {
+    const selectProject = state.projects.find(
+      (project) => project.id === state.projectStateStatus
+    );
 
-  function handleUserLoginPage(userName, userPassword) {
-    setProjectState((prevState) => {
-      let UserLoggedIn = false;
-      if (userName === "user" && userPassword === "12345") {
-        UserLoggedIn = true;
-        return {
-          ...prevState,
-          isUserLoggedIn: UserLoggedIn,
-        };
-      }
+    const projectIndex = state.projects.findIndex(
+      (project) => project.id === selectProject.id
+    );
+
+    const updatedProject = {
+      ...state.projects[projectIndex],
+      tasks: action.taskitems,
+    };
+
+    const finalUpdated = [
+      ...state.projects.slice(0, projectIndex),
+      updatedProject,
+      ...state.projects.slice(projectIndex + 1),
+    ];
+
+    return {
+      ...state,
+      projects: finalUpdated,
+    };
+  } else {
+    return {
+      ...state,
+    };
+  }
+}
+export default function ProjectContext({ children }) {
+  const [projectStateReducer, setProjectsDispatch] = useReducer(
+    ProjectsStateUseReducer,
+    {
+      projectStateStatus: undefined,
+      projects: [],
+      uniqueId: 0,
+      isUserLoggedIn: false,
+    }
+  );
+
+  function handleSaveData(projectDetails) {
+    setProjectsDispatch({
+      type: "ADD-PROJECT",
+      projectDetails: projectDetails,
     });
   }
 
   function updateProjectState(status, id = null) {
-    console.log(status, id, "coming");
-    setProjectState((prevState) => {
-      if (status == "delete" && id) {
-        const updatedProjects = prevState.projects.filter(
-          (project) => project.id !== id
-        );
-        return {
-          ...prevState,
-          projects: updatedProjects,
-          projectStateStatus: "returnDashboard",
-        };
-      } else {
-        return {
-          ...prevState,
-          projectStateStatus: status,
-        };
-      }
+    console.log(status, id);
+    if (status === "delete" && id !== "") {
+      setProjectsDispatch({
+        type: "UPDATE-PROJECT",
+        payload: {
+          status: status,
+          id: id,
+        },
+      });
+    } else {
+      setProjectsDispatch({
+        type: "UPDATE-PROJECT",
+        pageChange: status,
+      });
+    }
+  }
+
+  function handleUserLoginPage(userName, userPassword) {
+    let UserLoggedIn = false;
+    if (userName === "user" && userPassword === "12345") {
+      UserLoggedIn = true;
+    }
+    setProjectsDispatch({
+      type: "USER-LOGIN",
+      isUserLoggedIn: UserLoggedIn,
     });
   }
 
   function handleTasksData(tasksItems) {
-    console.log(tasksItems, "TasksRecieving");
-    setProjectState((prevState) => {
-      const selectProject = prevState.projects.find(
-        (project) => project.id === prevState.projectStateStatus
-      );
-      console.log(selectProject, "1");
-
-      const projectIndex = prevState.projects.findIndex(
-        (project) => project.id === selectProject.id
-      );
-      console.log(projectIndex, "2");
-
-      const updatedProject = {
-        ...prevState.projects[projectIndex],
-        tasks: tasksItems,
-      };
-      console.log(updatedProject, "Updated");
-
-      const finalUpdated = [
-        ...prevState.projects.slice(0, projectIndex),
-        updatedProject,
-        ...prevState.projects.slice(projectIndex + 1),
-      ];
-      console.log(finalUpdated, "final");
-
-      return {
-        ...prevState,
-        projects: finalUpdated,
-      };
-    });
-  }
-
-  function handleSaveData(projectDetails) {
-    setProjectState((prevState) => {
-      const newId = prevState.uniqueId + 1;
-      const newProject = {
-        ...projectDetails,
-        id: newId,
-      };
-      return {
-        ...prevState,
-        projects: [...prevState.projects, newProject],
-        uniqueId: newId,
-        projectStateStatus: "returnDashboard",
-      };
+    setProjectsDispatch({
+      type: "UPDATE-TASKS",
+      taskitems: tasksItems,
     });
   }
 
   const projectsData = {
-    ...projectState,
+    ...projectStateReducer,
     handleUserLoginPage,
     handleTasksData,
     handleSaveData,
